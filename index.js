501799892d26590275575ac75e175daa5341b7b8
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-04-04 23:01:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let num1 = 8
+let num2 = 2
+let sum = document.getElementById("sum-el")
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+function add() {
+    result = num1 + num2
+    sum.innerText = "Sum: " + result;
+}
+
+function subtract() {
+    result = num1 - num2
+    sum.innerText = "Sum: " + result;
+}
+
+function divide() {
+    result = num1 / num2
+    sum.innerText = "Sum: " + result;
+}
+
+function multiply() {
+    result = num1 * num2
+    sum.innerText = "Sum: " + result;
+}
+
+
